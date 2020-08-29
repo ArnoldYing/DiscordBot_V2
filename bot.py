ce862cdb4e2fd18768532758e1a4879f1c8f97c5
ArnoldYing <arnoldying825@gmail.com>
2020-08-28 23:44:52 -0700

    added bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
