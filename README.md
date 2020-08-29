fcc0701c0d702bb68bfaab5bb115134606a723ad
ArnoldYing <arnoldying825@gmail.com>
2020-08-29 00:24:39 -0700

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # DiscordBot_V2
+
+## Multi-function discord bot used to personalize and elevate any discord server
+### Planned features:
+
+1.  Stream notification
+2.  Bot Commands (music, interactions)
+3.  Ping system
+
+### and more features coming soon!
--- bot.py
+++ bot.py
@@ -8,9 +8,12 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+game = discord.Game("with a fidget spinner")
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
 async def on_message(message):
